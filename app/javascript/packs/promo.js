a480be0272cd510375cbdb54c71ccdb099ab8a8c
Natasha <natasha.kim333@gmail.com>
2020-11-16 04:04:07 +0300

    WIP

---
+++ app/javascript/packs/promo.js
@@ -0,0 +1,15 @@
+let animSix  = anime({
+  targets: '.goods_floating',
+  keyframes: [
+    {translateY: 7},
+    {translateY: -7},
+    {translateY: 7},
+    {translateY: -7},
+    {translateY: 0}
+  ],
+  loop: true,
+  easing: 'cubicBezier(.5, .05, .1, .3)',
+  delay: 1000,
+  duration: 10000,
+  loop: true
+});
