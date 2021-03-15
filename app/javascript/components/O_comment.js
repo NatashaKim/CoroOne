8e7c11e1cbdd8c57a943eab755a5990a0623c97f
Natasha <natasha.kim333@gmail.com>
2021-03-15 23:05:43 +0300

    Комментарии без реплаев

--- app/javascript/components/O_comment.js
+++ app/javascript/components/O_comment.js
@@ -5,13 +5,25 @@ import M_commentform from "./M_commentform"
 import A_debug from "./A_debug"
 import {getuser} from './Api.js';
 class O_comment extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state={
+        author: null
+      }
+      getuser(this.props.comment.user_id).then((u)=>{
+        this.setState({author: u})
+      })
+
+}
   render () {
+
+
 
     let childComments = this.props.comments.filter(c => c.parent_id ==this.props.comment.id);
 
     return (
       <div className="Comment">
-      <M_user_info user={this.props.author} />
+      <M_user_info user={this.state.author} />
         <div className="Comment-text">
           {this.props.comment.body}
         </div>
@@ -23,10 +35,9 @@ class O_comment extends React.Component {
       <div>
       childComments length = {childComments.length}
       {childComments.map(c =>
-        <div>
+        <div className = "comment_container">
         <O_comment
         comment={c}
-        author={getuser(c.user_id)}
         post={this.props.post}
         comments={this.props.comments}
         currentUser={this.props.currentUser}/>
