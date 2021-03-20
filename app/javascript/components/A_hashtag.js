31e70ca3c5c613d04fe46b6d64579d23e514bf1e
Natasha <natasha.kim333@gmail.com>
2021-03-20 20:27:09 +0300

    хештеги

--- app/javascript/components/A_hashtag.js
+++ app/javascript/components/A_hashtag.js
@@ -3,8 +3,18 @@ import PropTypes from "prop-types"
 
 class A_hashtag extends React.Component {
     render() {
+      let myRe = new RegExp("#\\w+", "g");
+      let myArray = [];
+      let tag = null;
+      while((tag = myRe.exec(this.props.post.content)) != null){
+        myArray.push(tag[0]);
+      }
       return (
-        <span>{this.props.post.tags}</span>
+        <div>
+        {
+          myArray.map(mytag => (<p>{mytag}</p>))
+        }
+        </div>
       );
     }
   }
--- app/javascript/components/M_postview.js
+++ app/javascript/components/M_postview.js
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import M_category_nav from "./M_category_nav"
+import A_hashtag from "./A_hashtag"
 class M_postview extends React.Component {
   render() {
     let categoryTag='';
@@ -8,6 +9,14 @@ class M_postview extends React.Component {
     if (this.props.category) {
       categoryTag = <p>Category:{this.props.category.name}</p>;
     }
+
+    let myRe = new RegExp("#\\w+", "g");
+    let myArray = [];
+    let tag = null;
+    while((tag = myRe.exec("#abc, #haefg #hij")) != null){
+      myArray.push(tag[0]);
+    }
+
     return (
       <div>
         <h1>{this.props.post.title}</h1>
@@ -15,6 +24,7 @@ class M_postview extends React.Component {
 
             {categoryTag}
             <M_category_nav categories = {this.props.categories} post = {this.props.post} category = {this.props.category}/>
+            <A_hashtag post = {this.props.post}/>
             <p>ID: {`${this.props.post.id}`}</p>
             <p>Author: {`${this.props.post.author}`}</p>
             <p>Content: {`${this.props.post.content}`}</p>
