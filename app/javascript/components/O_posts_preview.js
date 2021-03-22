62523cd3c3ea6eb37f7bd8cbece0bf3d538455da
Natasha <natasha.kim333@gmail.com>
2021-03-22 23:48:46 +0300

    Начала переделывать посты

--- app/javascript/components/O_posts_preview.js
+++ app/javascript/components/O_posts_preview.js
@@ -16,13 +16,16 @@ class O_posts_preview extends React.Component {
 }
   render () {
     if (!this.state.posts) {return ""}
+    let postNumber = 0;
+    let size = 5;
 
     return (
       <div>
         <A_post_type post_type_id = {this.props.post_type_id} />
-        {this.state.posts.map(post => (
-          <M_post_preview key = {post.id} post = {post} category = {post.category} />
+        {this.state.posts.slice(0, size).map(post => (
+          <M_post_preview key = {post.id} post = {post} category = {post.category} post_number = {postNumber = postNumber + 1} />
         ))}
+        
       </div>
     );
   }
