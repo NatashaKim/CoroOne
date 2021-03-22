b674f694007ccb145f8e1e6a3d9bbfffb72a6c19
Natasha <natasha.kim333@gmail.com>
2021-03-22 11:39:52 +0300

    Поиск способа затемнить картинки

--- app/javascript/components/M_post_preview.js
+++ app/javascript/components/M_post_preview.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import A_category from "./A_category"
-import A_title from "./A_title"
+import A_post_title from "./A_post_title"
 class M_post_preview extends React.Component {
   render () {
     let imgsrc = '';
@@ -10,13 +10,17 @@ class M_post_preview extends React.Component {
     } else {
       imgsrc = this.props.post.image.thumb.url;
     }
+
+    var sectionStyle = {
+      width: "326px",
+      height: "488px",
+      borderRadius: "32px",
+      backgroundImage: `linear-gradient( rgba(4, 0, 94, 0.4), rgba(4, 0, 94, 0.4) ), url(${imgsrc})`
+    };
     return (
-      <div>
-      <A_category category = {this.props.post.category}/>
-      <A_title title = {this.props.post.title} />
-      <img
-        src = {imgsrc}
-      />
+      <div style={sectionStyle}>
+        <A_category category = {this.props.post.category}/>
+        <A_post_title post = {this.props.post}/>
       </div>
     );
   }
