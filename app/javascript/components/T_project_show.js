23356568b30f195face098f417e1092649119c07
Natasha <natasha.kim333@gmail.com>
2021-06-25 01:29:07 +0300

    Кусок проекта

--- app/javascript/components/T_project_show.js
+++ app/javascript/components/T_project_show.js
@@ -1,65 +1,61 @@
 import React from "react"
 import PropTypes from "prop-types"
-import O_article from "./O_article"
-import M_hashtags from "./M_hashtags"
-import A_postcover from "./A_postcover"
-import A_posttype_marker from "./A_posttype_marker"
-import O_puzzle_preview from "./O_puzzle_preview"
-import O_comment from "./O_comment"
-import M_commentform from "./M_commentform"
-import A_title from "./A_title"
+
+import M_project_side_info from "./M_project_side_info"
+import A_project_cover from "./A_project_cover"
+import M_project_header from "./M_project_header"
+import M_project_checklist from "./M_project_checklist"
+
 
 import {getpost_type} from './Api.js';
 import '../../assets/stylesheets/M_postview.scss'
 
-class T_article extends React.Component {
+class T_project_show extends React.Component {
 
   render() {
     return (
-      <div className="Article_page">
-        <div className="Article_image_header">
+      <div className="Project_page">
+
+        <div className="Project_image_header">
           <div className="Article_marker_holder">
-            <A_posttype_marker
-             post_type = {this.state.post_type}
+            <div className="Project_marker_placeholder">
+              В процессе
+            </div>
+          </div>
+          <A_project_cover
+            project = {this.props.project}
+          />
+        </div>
+
+
+
+
+        <div className="M_project_div">
+          <div className="M_project_left_section">
+            <M_project_side_info
+              project = {this.props.project}
             />
           </div>
-          <A_postcover
-            post = {this.props.post}
-          />
+
+          <div className="M_project_right_section">
+            <div>
+
+              <M_project_header
+                project = {this.props.project}
+                current_user_id = {this.props.current_user_id}
+                genres = {this.props.genres}
+              />
+
+              <M_project_checklist
+                project = {this.props.project}
+              />
+
+            </div>
+          </div>
         </div>
-        <O_article
-          post = {this.props.post}
-          current_user_id={this.props.current_user_id}
-        />
-        <M_hashtags
-          post = {this.props.post}
-        />
-        <div className="Article_puzzle_heading">
-          <A_title
-            headingColor = 'navy-blue'
-            headingStyle = "h3"
-            title = "Похожие статьи"
-          />
-        </div>
-        <O_puzzle_preview
-          post_type_id = {this.props.post.post_type_id}
-          post_number = "4"
-          puzzlePlace = "article"
-        />
-        <O_comment
-          comment = {this.props.comment}
-          author = {this.props.author}
-          post = {this.props.post}
-          comments = {this.props.comments}
-          currentUser = {this.props.currentUser}
-        />
-        <M_commentform
-          post_id = {this.props.post_id}
-          user = {this.props.user}
-        />
       </div>
     );
   }
 }
 
-export default T_article
+export default T_project_show
