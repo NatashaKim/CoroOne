172ac8e73b2aad9d84b0035f5e7e1269883f13e6
Natasha <natasha.kim333@gmail.com>
2021-06-26 03:46:53 +0300

    Вход / выход

--- app/javascript/components/M_navbar.js
+++ app/javascript/components/M_navbar.js
@@ -5,6 +5,12 @@ import A_navbar_icon from "./A_navbar_icon.js"
 
 class M_navbar extends React.Component {
       render() {
+        let first_link = '';
+        let second_link = '';
+        if (this.props.current_user_id == -1) {
+          first_link = <a href="/users/sign_up">Зарегистрироваться</a>, second_link = <a href="/users/sign_in">Войти</a>;
+        } else {
+          first_link = <a rel="nofollow" data-method="delete" href="/users/sign_out">Выйти</a>, second_link = <a href={`/users/${this.props.current_user_id}`}>Аккаунт</a>;}
       return (
         <div className="Navbar">
         <a href={'/posts'}>
@@ -19,6 +25,7 @@ class M_navbar extends React.Component {
           />
           <A_navbar_icon
             iconTypes = "bookmarks"
+            current_user = {this.props.current_user}
           />
           <div className="Acclist">
             <A_navbar_icon
@@ -27,14 +34,10 @@ class M_navbar extends React.Component {
             />
             <div className="Account_action">
              <div className="Sign">
-              <a>
-                Войти
-              </a>
+                {first_link}
              </div>
              <div className="Sign">
-              <a>
-                Зарегистрироваться
-              </a>
+              {second_link}
              </div>
             </div>
           </div>
